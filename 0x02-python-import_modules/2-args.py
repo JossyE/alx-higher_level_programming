f143f7619ad86e47c0159c7881816c232631dc0b
JossyE <joeyeffiong@gmail.com>
2023-01-11 09:43:36 -0800

    import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(length))
-        if length >= 1:
-            for i in range(1, len(argv)):
-                print("{}: {}".format(i, argv[i]))
-                i += 1
+    if length >= 1:
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
