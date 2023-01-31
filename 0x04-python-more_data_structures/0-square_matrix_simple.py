4a9b74cb1a646e5075c740248dd0cf4c0b103e6b
Josephine Effiong <105316743+JossyE@users.noreply.github.com>
2023-01-31 13:50:15 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in range(len(matrix)):
-            new_matrix.append([i ** 2 for i in matrix[row]])
-    return new_matrix
+    if matrix:
+        return[[i**2 for i in row] for row in matrix]
